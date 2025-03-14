0ceccd2c1a9ab6679dfca2f3d47873f64764ba89
h0xxln <alxl47@naver.com>
2025-03-14 09:41:48 +0900

    logout.jsp로 리턴

--- w_admin/src/main/java/adm_login/logout.java
+++ w_admin/src/main/java/adm_login/logout.java
@@ -7,14 +7,13 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-@WebServlet("/logout")
+
 public class logout extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
     
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+		 request.getRequestDispatcher("./logout.jsp").forward(request, response);
 	}
 
 }
