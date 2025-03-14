28947f989c1dbaadc8e186dcd4d2b441ca7f157a
SeongMoong <sm5414@naver.com>
2025-03-14 18:08:01 +0900

    ffffffffff

--- w_admin/src/main/webapp/notice_write.js
+++ w_admin/src/main/webapp/notice_write.js
@@ -1,9 +1,23 @@
-function writech(){
-	if(frm.subject.value==""){
+function writech(p){
+	switch(p){
+		case 1:
+
+		location.href="./notice_list.do";
+		break;
+		case 2:
+		location.href="./notice_list.do";
+		break;
+	}
+	
+	
+
+}
+
+
+/*		if(frm.subject.value==""){
 		alert("제목을 입력하셔야 합니다.");
 		frm.subject.focus();
-	}
-	else{
+		}
+		else{
 		frm.submit();
-	}
-}
+		}*/
