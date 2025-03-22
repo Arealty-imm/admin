8d77bfe2e52e0776f9513868e968926cd2a7df7b
h0xxln <alxl47@naver.com>
2025-03-22 09:20:36 +0900

    정규식 패턴 변경 (_ 언더바 추가)

--- w_admin/src/main/webapp/js/master_login.js
+++ w_admin/src/main/webapp/js/master_login.js
@@ -8,7 +8,7 @@ function master_login(){
         return;
     }
     // 비밀번호 확인 (8자 이상, 숫자, 문자, 특수문자 포함)
-    var passPattern = /^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*?&#])[A-Za-z\d@$!%*?&#+]{8,20}$/;
+   var passPattern = /^(?=.*[A-Za-z])(?=.*\d)(?=.*[\W_])[A-Za-z\d\W_]{8,20}$/;
 
     if (!passPattern.test(adm_pass)) {
         alert("비밀번호는 8자 이상, 숫자, 문자, 특수문자를 포함해야 합니다.");
